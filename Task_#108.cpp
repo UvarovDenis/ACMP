d369659ff01e9e4331e76cec765961b4e9beb5dc
Denis <denis@MacBook-Pro13.local>
2018-11-07 13:42:07 +0300

    Task #108
    https://acmp.ru/index.asp?main=task&id_task=108

---
+++ Task_#108.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    int value;
+    cin >> value;
+    cout << value;
+    return 0;
+}
