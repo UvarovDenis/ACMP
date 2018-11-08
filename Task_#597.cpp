cfb4d232889893102d0f710be89f4702d7695776
Denis <denis@MacBook-Pro13.local>
2018-11-08 20:41:58 +0300

    Task #597
    https://acmp.ru/index.asp?main=task&id_task=597

---
+++ Task_#597.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    int r1,r2,r3;
+    cin >> r1 >> r2 >> r3;
+    if (r1 >= r2 + r3)
+    { cout << "YES"; }
+    else { cout << "NO"; }
+    return 0;
+}
